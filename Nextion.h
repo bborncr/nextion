8e9132652c7b8fbdf1292004c5b5aaa05b4ec175
Andrej Kostrov <github@wmod.net>
2017-10-29 22:31:57 +0100

    fix setComponentValue to transfer long values. the number component accept 32bit values. the int in arduino is 16 bit only

--- Nextion.h
+++ Nextion.h
@@ -70,7 +70,7 @@ class Nextion{
 
   uint8_t buttonOnOff(String find_component, String unknown_component, uint8_t pin, int btn_prev_state);
 
-  boolean setComponentValue(String component, int value);
+  boolean setComponentValue(String component, long value);
 
   //boolean ack(void);//Deprecated
 
